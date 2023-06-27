08aa9940f4a72e7bb815a4cafa5bc8ab80fc7db8
Stacy1502 <99914407+Stacy1502@users.noreply.github.com>
2023-06-27 15:18:10 -0400

    js files in header get requested

--- node_server/controllers/standalone_template.js
+++ node_server/controllers/standalone_template.js
@@ -1,4 +1,5 @@
 // Standalone code for Circles World
+// standalone.js will fill in on server start
 
 // Adding Circles code parts when html body is done loading
 // -------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -14,7 +15,7 @@ window.addEventListener('load', function()
     
     for (let i = 0; i < length; i++)
     {
-        if (userScripts[i].src !== 'https://ee5d-142-161-79-245.ngrok-free.app/standalone-circles')
+        if (userScripts[i].src !== 'central-domain' + '/standalone-circles')        // central-domain is replaced
         {
             let splitSRC = userScripts[i].src.split('/');
             userScriptsFiltered.push('scripts/' + splitSRC[splitSRC.length - 1]);
@@ -29,7 +30,7 @@ window.addEventListener('load', function()
     }
 
     // Adding code to the head of the HTML document
-    document.getElementsByTagName('head')[0].innerHTML += 'start-scripts-part';
+    document.getElementsByTagName('head')[0].innerHTML += 'start-scripts-part';     // start-scripts-part is replaced
 
     // Reloading user scripts
     for (const script of userScriptsFiltered)
@@ -43,13 +44,15 @@ window.addEventListener('load', function()
     // Adding code to the body of the HTML document
     let bodyHTML = document.getElementsByTagName('body')[0].innerHTML;
 
-    bodyHTML = bodyHTML.replace(/<circles-start-ui(\s+)?>(\s+)?<\/circles-start-ui>/i, 'start-ui-part');
-    bodyHTML = bodyHTML.replace(/circles_scene_properties/i, 'scene-part');
-    bodyHTML = bodyHTML.replace(/<circles-assets(\s+)?>(\s+)?<\/circles-assets>/i, 'assets-part');
-    bodyHTML = bodyHTML.replace(/<circles-manager-avatar(\s+)?>(\s+)?<\/circles-manager-avatar>/i, 'avatar-part');
-    bodyHTML = bodyHTML.replace(/<circles-end-scripts(\s+)?>(\s+)?<\/circles-end-scripts>/i, 'end-part');
+    bodyHTML = bodyHTML.replace(/<circles-start-ui(\s+)?>(\s+)?<\/circles-start-ui>/i, 'start-ui-part');                // start-ui-part is replaced
+    bodyHTML = bodyHTML.replace(/circles_scene_properties/i, 'scene-part');                                             // scene-part is replaced
+    bodyHTML = bodyHTML.replace(/<circles-assets(\s+)?>(\s+)?<\/circles-assets>/i, 'assets-part');                      // assets-part is replaced
+    bodyHTML = bodyHTML.replace(/<circles-manager-avatar(\s+)?>(\s+)?<\/circles-manager-avatar>/i, 'avatar-part');      // avatar-part is replaced
+    bodyHTML = bodyHTML.replace(/<circles-end-scripts(\s+)?>(\s+)?<\/circles-end-scripts>/i, 'end-part');               // end-part is replaced
 
     document.getElementsByTagName('body')[0].innerHTML = bodyHTML;
 });
+
+//
 // -------------------------------------------------------------------------------------------------------------------------------------------------------
 
