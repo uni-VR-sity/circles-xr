e66239bc9d0c1eb92da60fdebf4b60ebee77c0c0
Stacy1502 <99914407+Stacy1502@users.noreply.github.com>
2023-07-06 18:59:38 -0400

    database created for user uploaded content

---
+++ node_server/models/uploads.js
@@ -0,0 +1,39 @@
+'use strict';
+
+//database
+const mongoose = require('mongoose');
+
+const UploadSchema = new mongoose.Schema({
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'users',
+      },
+    displayName: {
+        type:       String,
+        unique:     false,
+        required:   true,
+        trim:       true
+      },
+    name: {
+        type:       String,
+        unique:     true,
+        required:   true,
+        trim:       true
+      },
+    url: {
+        type:       String,
+        unique:     true,
+        required:   true,
+        trim:       true
+      },
+    type: {
+        type:       String,
+        unique:     false,
+        required:   true,
+        trim:       true
+      },
+});
+
+const Uploads = mongoose.model('uploads', UploadSchema);
+
+module.exports = Uploads;
