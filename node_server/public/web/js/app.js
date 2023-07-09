390edfd0936f5a3b6c431d599cfd5d18208e756a
Stacy1502 <99914407+Stacy1502@users.noreply.github.com>
2023-07-09 16:07:53 -0400

    Revert "WILL UNDO"
    This reverts commit e362a1a14a6a3529c3c336c9e49b6155f702c04a.

---
+++ node_server/public/web/js/app.js
@@ -0,0 +1,118 @@
+window.onload = function () {
+  //have something in the group field when opening page
+  //let's not as "explore" will be used if field is blank
+  // if (typeof MagicLinkGroup !== 'undefined') {
+  //   autogenerateGroupName(MagicLinkGroup, 4);
+  // }
+
+  let popover = new Popover();
+  const cps = document.querySelectorAll('.colorPicker'); //from here - https://github.com/tovic/color-picker 
+
+  if (!cps.length) {
+    return;
+  }
+
+  for (let i = 0; i < cps.length; i++) {
+    let picker = new CP(cps[i]);
+
+    picker.on("change", function (color) {
+      let v = CP.HEX2RGB(color);
+      //v = alpha.value == 1 ? 'rgb(' + v.join(', ') + ')' : 'rgba(' + v.join(', ') + ', ' + alpha.value.replace(/^0\./, '.') + ')';
+      v = 'rgb(' + v.join(', ') + ')';
+      this.target.value = v;
+      this.target.style.backgroundColor = v;
+    });
+
+    //update on direct text change ... not great UX but will do for now
+    let update = function () {
+      picker.set(this.value).enter();
+    }
+
+    picker.target.oncut = update;
+    picker.target.onpaste = update;
+    picker.target.onkeyup = update;
+    picker.target.oninput = update;
+  }
+}
+
+class Popover {
+  constructor() {
+    this.activator = document.querySelector('.topbar-header__popover-activator');
+    this.popover = document.querySelector('.topbar-popover');
+    this.wrapper = document.querySelector('.topbar-header__popover-wrapper');
+    this.activator.addEventListener('click', this.togglePopover.bind(this));
+    document.addEventListener('click', this.closePopover.bind(this));
+  }
+
+  togglePopover() {
+    this.popover.classList.toggle('topbar-popover--is-visible');
+  }
+
+  closePopover(evt) {
+    if (!this.wrapper.contains(evt.target) ) {
+      this.popover.classList.remove('topbar-popover--is-visible');
+    }
+  }
+}
+
+//********** timer functionality
+let g_intervalTimer = null;
+
+function startCoundown(numMS, textId) {
+  clearInterval(g_intervalTimer); //remove if already counting down
+
+  let currTime = new Date();
+  let countDownDate = new Date(currTime.getTime() + numMS).getTime();
+
+  // Update the count down every 1 second
+  g_intervalTimer = setInterval(function() {
+    // Get today's date and time
+    let now = new Date().getTime();
+      
+    // Find the distance between now and the count down date
+    let time_distance = countDownDate - now;
+      
+    // Time calculations for days, hours, minutes and seconds
+    let seconds = Math.floor(time_distance / 1000);
+    let minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    let hours = Math.floor(minutes / 60);
+    minutes = minutes % 60;
+    let days = Math.floor(hours / 24);
+    hours = hours % 24;
+      
+    // Output the result in an element with id="demo"
+    document.querySelector('#' + textId).innerHTML = days + " days, " + hours + " hours, " + minutes + " minutes, " + seconds + " seconds";
+      
+    // If the count down is over, write some text 
+    if (time_distance < 0) {
+      clearInterval(g_intervalTimer);
+      document.querySelector('#' + textId).innerHTML = "EXPIRED";
+    }
+  }, 1000);
+}
+
+function copyText(copyTextElem) {
+  // Select the text field
+  copyTextElem.select(); 
+  copyTextElem.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field (need to use promises so this actually copies)
+  navigator.clipboard.writeText(copyTextElem.value).then(function() {
+    alert("Copied the magic link!");
+  });
+}
+
+// Hiding information about file and displaying delete confirmation
+function doubleCheckDelete_UploadFile(fileName)
+{
+    document.getElementById('info' + fileName).style.display = 'none';
+    document.getElementById('deleteConfirmation' + fileName).style.display = 'block';
+}
+
+// Canceling delete by hiding delete confirmation and displaying information about the file again
+function cancelDelete_UploadFile(fileName)
+{
+  document.getElementById('info' + fileName).style.display = 'block';
+  document.getElementById('deleteConfirmation' + fileName).style.display = 'none';
+}
