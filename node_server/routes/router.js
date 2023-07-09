c25ba7c7e7eee0a3864c34035e47d43198bb46cb
Stacy1502 <99914407+Stacy1502@users.noreply.github.com>
2023-07-09 16:13:50 -0400

    Revert "Merge branch 'central' into main"
    This reverts commit 9b7711da02c090cd563789bc54a58283445164de, reversing
    changes made to 390edfd0936f5a3b6c431d599cfd5d18208e756a.

--- node_server/routes/router.js
+++ node_server/routes/router.js
@@ -132,14 +132,6 @@ router.get('/delete-uploaded-content/:file_name', authenticated, controller.dele
 
 router.post('/upload-content', authenticated, controller.newContent);
 
-router.post('/add-server', authenticated, controller.addServer);
-
-router.get('/get-servers', controller.getServersList); // This is requested from outside servers and can not have authenticated access only
-
-router.get('/inactivate-server/:server_id', authenticated, controller.inactivateServer);
-router.get('/activate-server/:server_id', authenticated, controller.activateServer);
-router.get('/delete-server/:server_id', authenticated, controller.deleteServer);
-
 router.post('/create-user', authenticated, controller.createUser);
 router.post('/bulk-create-users', authenticated, controller.createUsersByFile);
 router.post('/change-usertype', authenticated, controller.updateUsertype);
