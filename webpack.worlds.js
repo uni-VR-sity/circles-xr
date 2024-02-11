892b9b76ffa9f4abe6cae4e5bd341befebd0e735
Stacy1502 <99914407+StacyG15@users.noreply.github.com>
2024-02-11 18:18:49 -0500

    Added overrides for networking and avatar

--- webpack.worlds.js
+++ webpack.worlds.js
@@ -16,20 +16,28 @@ if (env.error) {
 // Parse the dot configs so that things like false are boolean, not strings
 env = dotenvParseVariables(env.parsed);
 
-//read in parts content to insert
-let circles_header           =  fs.readFileSync('./src/webpack.worlds.parts/circles_header.part.html', 'utf8');
-let circles_basic_ui         =  fs.readFileSync('./src/webpack.worlds.parts/circles_basic_ui.part.html', 'utf8');
-let circles_enter_ui         =  fs.readFileSync('./src/webpack.worlds.parts/circles_enter_ui.part.html', 'utf8');
-let circles_scene_properties =  fs.readFileSync('./src/webpack.worlds.parts/circles_scene_properties.part.html', 'utf8');
-let circles_assets           =  fs.readFileSync('./src/webpack.worlds.parts/circles_assets.part.html', 'utf8');
-let circles_avatar           =  fs.readFileSync('./src/webpack.worlds.parts/circles_avatar_manager.part.html', 'utf8');
-let circles_end_scripts      =  fs.readFileSync('./src/webpack.worlds.parts/circles_end_scripts.part.html', 'utf8');
+// Read in parts content to insert (default parts)
+let circles_header              =  fs.readFileSync('./src/webpack.worlds.parts/circles_header.part.html', 'utf8');
+let circles_basic_ui            =  fs.readFileSync('./src/webpack.worlds.parts/circles_basic_ui.part.html', 'utf8');
+let circles_enter_ui            =  fs.readFileSync('./src/webpack.worlds.parts/circles_enter_ui.part.html', 'utf8');
+let circles_scene_properties    =  fs.readFileSync('./src/webpack.worlds.parts/circles_scene_properties.part.html', 'utf8');
+let circles_assets              =  fs.readFileSync('./src/webpack.worlds.parts/circles_assets.part.html', 'utf8');
+let circles_avatar              =  fs.readFileSync('./src/webpack.worlds.parts/circles_avatar_manager.part.html', 'utf8');
+let circles_end_scripts         =  fs.readFileSync('./src/webpack.worlds.parts/circles_end_scripts.part.html', 'utf8');
+
+// Read in parts content to insert (no networking parts)
+let circles_NN_scene_properties =  fs.readFileSync('./src/webpack.worlds.parts/noNetworking/circles_NN_scene_properties.part.html', 'utf8');
+let circles_NN_end_scripts      =  fs.readFileSync('./src/webpack.worlds.parts/noNetworking/circles_NN_end_scripts.part.html', 'utf8');
+
+// Read in parts content to insert (no avatar parts)
+let circles_NA_enter_ui         =  fs.readFileSync('./src/webpack.worlds.parts/noAvatar/circles_NA_enter_ui.part.html', 'utf8');
+let circles_NA_avatar           =  fs.readFileSync('./src/webpack.worlds.parts/noAvatar/circles_NA_avatar_manager.part.html', 'utf8');
 
 const nafAudioRegex   = new RegExp(/\{\{(\s+)?NAF_AUDIO(\s+)?\}\}/,   'gmi');
 const nafAdapterRegex = new RegExp(/\{\{(\s+)?NAF_ADAPTER(\s+)?\}\}/, 'gmi');
 const nafServerRegex  = new RegExp(/\{\{(\s+)?NAF_SERVER(\s+)?\}\}/,  'gmi');
 
-//insert env vars into parts
+// Insert env vars into parts
 circles_scene_properties = circles_scene_properties.toString().replace(nafAudioRegex,   env.NAF_AUDIO);
 circles_scene_properties = circles_scene_properties.toString().replace(nafAdapterRegex, env.NAF_ADAPTER);
 circles_scene_properties = circles_scene_properties.toString().replace(nafServerRegex,  env.NAF_SERVER);
@@ -60,7 +68,7 @@ module.exports = {
           to: './',
           transform (content, path) {
             if (path.endsWith('.html')) {
-              //insert new parts
+              // Insert new parts (default parts)
               content = content.toString();
               content = content.replace(/<circles-start-scripts(\s+)?\/>/i, circles_header);
               content = content.replace(/<circles-basic-ui(\s+)?\/>/i, circles_basic_ui);
@@ -69,6 +77,12 @@ module.exports = {
               content = content.replace(/<circles-assets(\s+)?\/>/i, circles_assets);
               content = content.replace(/<circles-manager-avatar(\s+)?\/>/i, circles_avatar);
               content = content.replace(/<circles-end-scripts(\s+)?\/>/i, circles_end_scripts);
+              // Insert new parts (no networking parts)
+              content = content.replace(/circles_NN_scene_properties/i, circles_NN_scene_properties);
+              content = content.replace(/<circles-NN-end-scripts(\s+)?\/>/i, circles_NN_end_scripts);
+              // Insert new parts (no avatar parts)
+              content = content.replace(/<circles-NA-start-ui(\s+)?\/>/i, circles_NA_enter_ui);
+              content = content.replace(/<circles-NA-manager-avatar(\s+)?\/>/i, circles_NA_avatar);
               //return content.toString().replace(janusServerRegex, env.JANUS_SERVER);
               return content;
             } else {
