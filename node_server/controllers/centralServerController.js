13ec23de4a02cac1ba8cc3ffc723d0f7a72c2fb9
Stacy1502 <99914407+StacyG15@users.noreply.github.com>
2024-02-14 22:16:25 -0500

    Museum games page updated

--- node_server/controllers/centralServerController.js
+++ node_server/controllers/centralServerController.js
@@ -274,103 +274,103 @@ const serveMuseumGames = async (req, res, next) =>
   var games = [];
 
   games[0] = {
-    name: 'Game',
-    credit: 'Test Studios',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'www.testergame.ca',
-    image: 'default-circle-profile.png',
-    contact: 'person@testStudios.ca',
+    name: "Asteroid Field (VR)",
+    credit: "------",
+    description: "------",
+    instructions: "------",
+    link: "------",
+    image: "default-circle-profile.png",
+    contact: "------",
   }
 
   games[1] = {
-    name: 'Game',
-    credit: 'Test Studios',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'www.testergame.ca',
-    image: 'default-circle-profile.png',
-    contact: 'person@testStudios.ca',
+    name: "Ambivalence",
+    credit: "Elis Joynes",
+    description: "------",
+    instructions: "------",
+    link: "https://elisvoid.itch.io/ambivalence",
+    image: "default-circle-profile.png",
+    contact: '-----',
   }
 
   games[2] = {
-    name: 'Game',
-    credit: 'Test Studios',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'www.testergame.ca',
-    image: 'default-circle-profile.png',
-    contact: 'person@testStudios.ca',
+    name: "Avian RumbLOL",
+    credit: "------",
+    description: "------",
+    instructions: "------",
+    link: "------",
+    image: "default-circle-profile.png",
+    contact: "------",
   }
 
   games[3] = {
-    name: 'Game',
-    credit: 'Test Studios',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'www.testergame.ca',
-    image: 'default-circle-profile.png',
-    contact: 'person@testStudios.ca',
+    name: "Deck of Laughs",
+    credit: "Tyson Moyes, Michael Dillabough, Jason Hein, David Dunkelman, Mathieu O'Brien",
+    description: "Tactical Card Comedy! Use your spell cards to cast jokes onto the crowd of gremlins to make them laugh. Beware, the hexler will cast his own spells and end the laughter.\nBug note: In the end screen, if your mouse disappears after clicking, hit TAB on the keyboard, your mouse should reappear.",
+    instructions: "Extract the files from the zip. In-game, click on the card you wish to read & select. Click on the location in the crowd to cast the spell. Right click to cancel the spell and to deselect the card. When you are done your turn, click the 'end turn' button.",
+    link: "https://skypyre.itch.io/deck-of-laughs",
+    image: "DeckOfLaughsCover.png",
+    contact: "general@skypyre.com",
   }
 
   games[4] = {
-    name: 'Game',
-    credit: 'Test Studios',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'www.testergame.ca',
-    image: 'default-circle-profile.png',
-    contact: 'person@testStudios.ca',
+    name: "Just a Jester Jesting",
+    credit: "------",
+    description: "Can you perform enough tricks to survive the king's wrath?\nA 2D and 3D fusion game where you only need a mouse to play. Help the jester be funny for the court\nSurvive as long as you can while keeping all your lives (hearts), the fast paced mini games in the bottom half of the screen will test your skill. Inspired by Nintendo DS games likes Wario Ware and Rhythm Heaven. Keep up!",
+    instructions: "Mouse: Use the left click to interact with elements on the bottom half of the screen\nClick fast, drag, and time your clicks to stay alive.",
+    link: "https://dcole.itch.io/just-a-jester-jesting",
+    image: "default-circle-profile.png",
+    contact: "------",
   }
 
   games[5] = {
-    name: 'Game',
-    credit: 'Test Studios',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'www.testergame.ca',
-    image: 'default-circle-profile.png',
-    contact: 'person@testStudios.ca',
+    name: "Sea Barks",
+    credit: "Anastacia Gorbenko, Msgana Ocabazgi, Sam Lamoureux, Zoe Prevost",
+    description: "In this underwater adventure, you play the role of Neptune, an eager puppy looking to please his owner at his new underwater farm SeaBarks. Neptune is responsible for putting away the sea creatures, and protecting them from hungry sharks. Get the sea creatures in to correct pens and you will be rewarded with treats.",
+    instructions: "Use your keyboard arrows to move around, and the space bar to bark!\nGuide sea creatures into their pens, and the catch? Neptune holds himself to good boy standards and wants to guide a minimum number of sea creatures to safety (shown on each pen). Neptune's hard work does not go unnoticed, he receives a treat before embarking on his next duty.\nBarking can be used on the sea creatures and the shark to yield different results. For the sea creatures, barking has different effects on each creature (find out more on hints screen) and barking at the shark scares them away when they are attacking.\nThe shark will think about its meal choice for a few seconds before they attack, be prepared to defend that creature! If the shark eats too many and you can no longer meet the farm’s minimum requirements, it's level over. Don’t worry, Neptune's owner always gives him another shot!",
+    link: "https://stacy-g.itch.io/sea-barks",
+    image: "SeaBarksCover.png",
+    contact: "------",
   }
 
   games[6] = {
-    name: 'Game',
-    credit: 'Test Studios',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'www.testergame.ca',
-    image: 'default-circle-profile.png',
-    contact: 'person@testStudios.ca',
+    name: "Shooting range (VR)",
+    credit: "Anastacia Gorbenko",
+    description: "------",
+    instructions: "------",
+    link: "------",
+    image: "ShootingRangeCover.png",
+    contact: "------",
   }
 
   games[7] = {
-    name: 'Game',
-    credit: 'Test Studios',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'www.testergame.ca',
-    image: 'default-circle-profile.png',
-    contact: 'person@testStudios.ca',
+    name: "Super Laugh Bros",
+    credit: "------",
+    description: "You're jesters tasked with pleasing the Lord of the land... with laughter! Please his requests for humor, but stay out of his way if he runs at you, or you'll be pushed out of the arena.",
+    instructions: "Controller controls:\nMove: left stick\nJump: Xbox A / Sony cross / Nintendo B\nAttack: Xbox X / Sony square / Nintendo Y\n\nKeyboard controls:\nMove: WASD / IJKL\nJump: E / U\nAttack: Q / O",
+    link: "https://globalgamejam.org/games/2024/super-laugh-bros-6",
+    image: "default-circle-profile.png",
+    contact: "------",
   }
 
   games[8] = {
-    name: 'Game',
-    credit: 'Test Studios',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'www.testergame.ca',
-    image: 'default-circle-profile.png',
-    contact: 'person@testStudios.ca',
+    name: "Wattson's Way Home",
+    credit: "------",
+    description: "------",
+    instructions: "------",
+    link: "https://alexdinobile.itch.io/wattsons-way-home",
+    image: "default-circle-profile.png",
+    contact: "------",
   }
 
   games[9] = {
-    name: 'Game',
-    credit: 'Test Studios',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    link: 'www.testergame.ca',
-    image: 'default-circle-profile.png',
-    contact: 'person@testStudios.ca',
+    name: "Wend",
+    credit: "Chrita B",
+    description: "Wend is a single player, top-down perspective game about exploration where you, the player, will navigate a complex labyrinth, lighting your way with a single torch as you collect relics and wander towards the center. The catch, however, is that the brightness and size of your torch light is dependent on your state of motion",
+    instructions: "Controller:\nUse the left stick to move around the labyrinth\nUse the right stick to move the camera (only enabled when you're standing still!)\nPress 'A' to pick up and drop gems, and to collect relics\nPress Start to pause\n\nKeyboard Controls:\nWASD to move\nArrow keys to move camera\nB key for picking up/dropping objects and any other action button prompts\nTAB to pause (arrow keys to navigate menus while paused)",
+    link: "https://christabuttera.itch.io/wend",
+    image: "default-circle-profile.png",
+    contact: "------",
   }
 
   res.render(path.resolve(__dirname + '/../public/web/views/CENTRAL_SERVER/museum-games'), {
