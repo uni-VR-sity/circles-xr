ad2579aedd6bfeeeb8d39fdfed61ef2b1b6afa6e
Stacy1502 <99914407+StacyG15@users.noreply.github.com>
2024-03-12 22:46:44 -0400

    museum games page updated

--- node_server/controllers/centralServerController.js
+++ node_server/controllers/centralServerController.js
@@ -274,7 +274,8 @@ const serveMuseumGames = async (req, res, next) =>
   var games = [];
 
   games[0] = {
-    name: "Asteroid Field (VR)",
+    name: "Asteroid Field",
+    platform: "VR",
     credit: "CirclesXR",
     description: "This is a mini-game demo for the Circles XR framework. Avoid the asteroids as you navigate through the Asteroid Field!",
     instructions: "<subtitle>VR Headset:\nEnter \"VR Mode\" to begin the game. Avoid the asteroids by jumping. Crouching, and moving side to side.\n\n<subtitle>Desktop:\nUse the WASD or arrow keys to move around through space.",
@@ -289,6 +290,7 @@ const serveMuseumGames = async (req, res, next) =>
 
   games[1] = {
     name: "Ambivalence",
+    platform: "WEB",
     credit: "Elis Joynes, Dandan Tao, Kylie Duck, Emily Chen",
     description: "Ambivalence is a puzzle platformer that plays on there being two halves of one whole puzzle that you switch between in order to get to the end.  ***Flashing Lights***",
     instructions: "All you need to play is your keyboard and mouse. We also suggest that you have your volume on as there are sound effects.",
@@ -301,7 +303,49 @@ const serveMuseumGames = async (req, res, next) =>
   }
 
   games[2] = {
+    name: "Sea Barks",
+    platform: "WEB",
+    credit: "Anastacia Gorbenko, Msgana Ocabazgi, Sam Lamoureux, Zoe Prevost",
+    description: "In this underwater adventure, you play the role of Neptune, an eager puppy looking to please his owner at his new underwater farm SeaBarks. Neptune is responsible for putting away the sea creatures, and protecting them from hungry sharks. Get the sea creatures in to correct pens and you will be rewarded with treats.",
+    instructions: "<list>Use your keyboard arrows to move around, and the space bar to bark!\n<list>Guide sea creatures into their pens, and the catch? Neptune holds himself to good boy standards and wants to guide a minimum number of sea creatures to safety (shown on each pen). Neptune's hard work does not go unnoticed, he receives a treat before embarking on his next duty.\n<list>Barking can be used on the sea creatures and the shark to yield different results. For the sea creatures, barking has different effects on each creature (find out more on hints screen) and barking at the shark scares them away when they are attacking.\n<list>The shark will think about its meal choice for a few seconds before they attack, be prepared to defend that creature! If the shark eats too many and you can no longer meet the farm’s minimum requirements, it's level over. Don’t worry, Neptune's owner always gives him another shot!",
+    link: "https://stacy-g.itch.io/sea-barks",
+    image: "SeaBarksCover.png",
+  }
+
+  games[3] = {
+    name: "Shooting Range",
+    platform: "WEB",
+    credit: "Anastacia Gorbenko",
+    description: "Practice your aim in this space-themed shooting range!",
+    instructions: "When you enter, choose your colour and begin. Look around using your mouse and shoot by pressing the spacebar.",
+    link: "https://uni-vr-sity.ca/Shooting-Range",
+    image: "ShootingRangeCover.png",
+    contact: "anastaciagorbenko@yahoo.ca",
+  }
+
+  games[4] = {
+    name: "Super Laugh Bros",
+    platform: "WEB",
+    credit: "Forest Kristoffer Ziven Anderson, Matt Penny, Matt Diener",
+    description: "You're jesters tasked with pleasing the Lord of the land... with laughter! Please his requests for humor, but stay out of his way if he runs at you, or you'll be pushed out of the arena.",
+    instructions: "<subtitle>Controller Controls:\n<list>Move: Left stick\n<list>Jump: Xbox A / Sony cross / Nintendo B\n<list>Attack: Xbox X / Sony square / Nintendo Y\n\n<subtitle>Keyboard Controls:\n<list>Move: WASD / IJKL\n<list>Jump: E / U\n<list>Attack: Q / O",
+    link: "https://mwpenny.github.io/GGJ2024",
+    image: "SuperLaughBrosCover.png",
+  }
+
+  games[5] = {
+    name: "Wattson's Way Home",
+    platform: "WEB",
+    credit: "Alex Di Nobile, Cole McMullin, Emma Souannhaphanh, Cohen Ly",
+    description: "Wattson’s Way Home is a 2D platformer game about helping a little spark find his way home. One day during a thunderstorm, Wattson fell out of a lightning bolt and was sent crashing into an unsuspecting house. Lost in a strange place filled with dust bunnies and leaky pipes, Wattson only knows that he needs to make it back home. Help find Wattson’s Way Home!",
+    instructions: "Use arrow keys to move, Space to jump, E/Enter to interact",
+    link: "https://alexdinobile.itch.io/wattsons-way-home",
+    image: "WattsonsWayHomeCover.png",
+  }
+
+  games[6] = {
     name: "Avian RumbLOL",
+    platform: "Windows EXE",
     credit: "Patrick Boyer",
     description: "In Avian RumbLOL, you're a bird trying to find letters to spell out \"LOL\", \"LMAO\", and \"ROFL\" before anyone else does. Be the first to get all the letters in the right order, and you win!",
     instructions: "<subtitle>Controls:\n<list>For the Chicken: Use the \"WASD\" keys to move around.\n<list>For the Pigeon: Use the arrow keys (← ↑ → ↓) to fly in different directions.\n<list>Got a Gamepad? You can use it to control the chicken if it's connected.\n<list>Oops, Need a Do-Over? Press \"P\" to start the game again.\n<list>All Done Playing? Press \"Esc\" to quit the game.\n\n<subtitle>Rules:\n<list>Wrap-Around Magic: Your bird can magically pop from one side of the screen to the other!\n<list>Letter Hunt: Grab letters as they pop up. But watch out! Grabbing a letter makes a grumpy duck come after you.\n<list>Duck Trouble: If a grumpy duck catches you, you'll lose your letters and have to start over.\n<list>Invincible Bird: After a duck bumps into you, you'll be invincible for a short time. You'll know because your bird will look really sad but don't worry, it can't be hurt during this time.\n<list>Speedy Feathers: Every time you complete a word sequence, you get a little faster. Zoom zoom!\n<list>Bumping Birds: The chicken and pigeon can bump into each other. It's all part of the fun!",
@@ -314,8 +358,9 @@ const serveMuseumGames = async (req, res, next) =>
     }],
   }
 
-  games[3] = {
+  games[7] = {
     name: "Deck of Laughs",
+    platform: "Windows EXE",
     credit: "Tyson Moyes, Michael Dillabough, Jason Hein, David Dunkelman, Mathieu O'Brien",
     description: "Tactical Card Comedy! Use your spell cards to cast jokes onto the crowd of gremlins to make them laugh. Beware, the hexler will cast his own spells and end the laughter.\nBug note: In the end screen, if your mouse disappears after clicking, hit TAB on the keyboard, your mouse should reappear.",
     instructions: "Extract the files from the zip. In-game, click on the card you wish to read & select. Click on the location in the crowd to cast the spell. Right click to cancel the spell and to deselect the card. When you are done your turn, click the \"end turn\" button.",
@@ -324,8 +369,9 @@ const serveMuseumGames = async (req, res, next) =>
     contact: "general@skypyre.com",
   }
 
-  games[4] = {
+  games[8] = {
     name: "ICE - Make me laugh for Mental Health",
+    platform: "Windows EXE",
     credit: "Avery Babineau",
     description: "This game was designed at the 2024 Global Game Jam. The theme for the game jam was \"Make Me Laugh\". This game is to promote and remind people how laughter can be used to improve mental health and make you feel better. The goal of this game is to make the monster (aka ICE) laugh and improve his mental health by collecting \"Jokens\" throughout the city of New York. Be careful, because at the beginning the monster is angry will not like when you get too close to him.",
     instructions: "You play the game by using the WASD keys to move in different directions, and the mouse to look around. Space bar is to jump, SHIFT key is to sprint. Remember to collect the coins (a.k.a. Jokens).",
@@ -334,8 +380,9 @@ const serveMuseumGames = async (req, res, next) =>
     contact: "av.babineau@gmail.com",
   }
 
-  games[5] = {
+  games[9] = {
     name: "Just a Jester Jesting",
+    platform: "Windows EXE",
     credit: "Adel Manji, Danielle Cole, Everett O’man, Marcos Angelos Santos, Nicolas Teriault",
     description: "Can you perform enough tricks to survive the king's wrath?\nA 2D and 3D fusion game where you only need a mouse to play. Help the jester be funny for the court\nSurvive as long as you can while keeping all your lives (hearts), the fast paced mini games in the bottom half of the screen will test your skill. Inspired by Nintendo DS games likes Wario Ware and Rhythm Heaven. Keep up!",
     instructions: "Mouse: Use the left click to interact with elements on the bottom half of the screen. Click fast, drag, and time your clicks to stay alive.",
@@ -349,8 +396,9 @@ const serveMuseumGames = async (req, res, next) =>
 
   }
 
-  games[6] = {
+  games[10] = {
     name: "Rooting for Carrots",
+    platform: "Windows EXE",
     credit: "Avery Babineau",
     description: "Rooting for carrots is a rpg-based farming simulator created for the 2022 Global Game Jam. The game jam theme was roots.",
     instructions: "Use the arrow keys to move. Find the garden. In the garden, left click to plant, watch the carrots grow and right click to harvest the carrots.",
@@ -358,45 +406,9 @@ const serveMuseumGames = async (req, res, next) =>
     image: "RootingForCarrotsCover.png",
   }
 
-  games[7] = {
-    name: "Sea Barks",
-    credit: "Anastacia Gorbenko, Msgana Ocabazgi, Sam Lamoureux, Zoe Prevost",
-    description: "In this underwater adventure, you play the role of Neptune, an eager puppy looking to please his owner at his new underwater farm SeaBarks. Neptune is responsible for putting away the sea creatures, and protecting them from hungry sharks. Get the sea creatures in to correct pens and you will be rewarded with treats.",
-    instructions: "<list>Use your keyboard arrows to move around, and the space bar to bark!\n<list>Guide sea creatures into their pens, and the catch? Neptune holds himself to good boy standards and wants to guide a minimum number of sea creatures to safety (shown on each pen). Neptune's hard work does not go unnoticed, he receives a treat before embarking on his next duty.\n<list>Barking can be used on the sea creatures and the shark to yield different results. For the sea creatures, barking has different effects on each creature (find out more on hints screen) and barking at the shark scares them away when they are attacking.\n<list>The shark will think about its meal choice for a few seconds before they attack, be prepared to defend that creature! If the shark eats too many and you can no longer meet the farm’s minimum requirements, it's level over. Don’t worry, Neptune's owner always gives him another shot!",
-    link: "https://stacy-g.itch.io/sea-barks",
-    image: "SeaBarksCover.png",
-  }
-
-  games[8] = {
-    name: "Shooting Range",
-    credit: "Anastacia Gorbenko",
-    description: "Practice your aim in this space-themed shooting range!",
-    instructions: "When you enter, choose your colour and begin. Look around using your mouse and shoot by pressing the spacebar.",
-    link: "https://uni-vr-sity.ca/Shooting-Range",
-    image: "ShootingRangeCover.png",
-    contact: "anastaciagorbenko@yahoo.ca",
-  }
-
-  games[9] = {
-    name: "Super Laugh Bros",
-    credit: "Forest Kristoffer Ziven Anderson, Matt Penny, Matt Diener",
-    description: "You're jesters tasked with pleasing the Lord of the land... with laughter! Please his requests for humor, but stay out of his way if he runs at you, or you'll be pushed out of the arena.",
-    instructions: "<subtitle>Controller Controls:\n<list>Move: Left stick\n<list>Jump: Xbox A / Sony cross / Nintendo B\n<list>Attack: Xbox X / Sony square / Nintendo Y\n\n<subtitle>Keyboard Controls:\n<list>Move: WASD / IJKL\n<list>Jump: E / U\n<list>Attack: Q / O",
-    link: "https://globalgamejam.org/games/2024/super-laugh-bros-6",
-    image: "SuperLaughBrosCover.png",
-  }
-
-  games[10] = {
-    name: "Wattson's Way Home",
-    credit: "Alex Di Nobile, Cole McMullin, Emma Souannhaphanh, Cohen Ly",
-    description: "Wattson’s Way Home is a 2D platformer game about helping a little spark find his way home. One day during a thunderstorm, Wattson fell out of a lightning bolt and was sent crashing into an unsuspecting house. Lost in a strange place filled with dust bunnies and leaky pipes, Wattson only knows that he needs to make it back home. Help find Wattson’s Way Home!",
-    instructions: "Use arrow keys to move, Space to jump, E/Enter to interact",
-    link: "https://alexdinobile.itch.io/wattsons-way-home",
-    image: "WattsonsWayHomeCover.png",
-  }
-
   games[11] = {
     name: "Wend",
+    platform: "Windows EXE",
     credit: "Christa Buttera, Matt Donoghue, Quinn Hentschel, Heather Hennessey",
     description: "Wend is a single player, top-down perspective game about exploration where you, the player, will navigate a complex labyrinth, lighting your way with a single torch as you collect relics and wander towards the center. The catch, however, is that the brightness and size of your torch light is dependent on your state of motion: the torch burns low and small while you run through the labyrinth, and burns bright and large while you stand still. Luminescent gems can be found throughout the labyrinth and allow you to mark your way as you proceed along the dark and mysterious paths. Uncover the mysteries of these unexplored depths as you wend your way through the labyrinth, and recover ancient relics to discover the secrets that lie at its center.",
     instructions: "You, the player, have the ability to control your torch light through the game's movement mechanics: while moving, only a small radius around your avatar is illuminated. While stationary, however, your torch is raised automatically to expand this radius to the neighbouring pathways to view a greater portion of the labyrinth. While playing, you can discover and collect emissive gems hidden throughout the labyrinth to help you find your way. These gems illuminate a small area around themselves, and, as such, can be placed anywhere in the labyrinth to create strategic paths to specific areas or landmarks that you deem significant.\n\n<subtitle>Controller Controls:\nUse the left stick to move around the labyrinth\nUse the right stick to move the camera (only enabled when you're standing still!)\nPress 'A' to pick up and drop gems, and to collect relics\nPress Start to pause\n\n<subtitle>Keyboard Controls:\nWASD to move\nArrow keys to move camera\nB key for picking up/dropping objects and any other action button prompts\nTAB to pause (arrow keys to navigate menus while paused)",
