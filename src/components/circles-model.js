03bdbc031fb2ebf380f7c6b22826874aba66d299
Anastacia Gorbenko <99914407+StacyG15@users.noreply.github.com>
2025-05-02 17:32:48 -0400

    fbx loader added to asset loading

--- src/components/circles-model.js
+++ src/components/circles-model.js
@@ -12,7 +12,26 @@ AFRAME.registerComponent('circles-model',
         this.modelMesh = null;
         this.complete = false;
 
-        // Loading model
+        // Loading model based on file type
+        if (schema.endsWith('.gltf') || schema.endsWith('.glb'))
+        {
+            this.gltfLoader();
+        }
+        else if (schema.endsWith('.fbx'))
+        {
+            this.fbxLoader();
+        }
+        else
+        {
+            console.log('circles-model: ' + schema + ' invalid file type');
+        }
+    },
+    // Loading gltf or glb model
+    gltfLoader: function()
+    {
+        const element = this.el;
+        const schema = this.data;
+
         const gltfLoader = new THREE.GLTFLoader();
 
         gltfLoader.load(schema, (gltf) => 
@@ -27,7 +46,26 @@ AFRAME.registerComponent('circles-model',
             this.complete = true;
         });
     },
+    // Loading fbx model
+    fbxLoader: function()
+    {
+        const element = this.el;
+        const schema = this.data;
 
+        const fbxLoader = new THREE.FBXLoader();
+
+        fbxLoader.load(schema, (fbx) => 
+        {
+            this.modelMesh = fbx;
+            this.complete = true;
+        },
+        undefined,
+        (error) =>
+        {
+            console.log('circles-model: ' + schema + " not found");
+            this.complete = true;
+        });
+    },
     // Returning if model is loaded
     isComplete: function()
     {
