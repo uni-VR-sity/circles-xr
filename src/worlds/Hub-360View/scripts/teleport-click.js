59d7d760cb0980fe35563442dd323af795bc5972
Jason <124936478+RoboRaccoon@users.noreply.github.com>
2025-07-04 20:05:51 -0400

    added moving teleport pads!

---
+++ src/worlds/Hub-360View/scripts/teleport-click.js
@@ -0,0 +1,39 @@
+AFRAME.registerComponent('teleport-click', {
+	init: function () {
+    // Use events to figure out what raycaster is listening so we don't have to
+    // hardcode the raycaster.
+    this.el.addEventListener('raycaster-intersected', evt => {
+        this.raycaster = evt.detail.el;
+    });
+    this.el.addEventListener('raycaster-intersected-cleared', evt => {
+      this.raycaster = null;
+    });
+
+    // mkaing the teleport pad
+    //create a teleport pad, then we'll move it to the interseting point
+    let sceneEl = document.querySelector('a-scene');
+    let newLocation = document.createElement('a-entity');
+    newLocation.setAttribute('id', 'teleport-cursor');
+    newLocation.setAttribute('circles-checkpoint', {});
+    sceneEl.appendChild(newLocation);
+  },
+
+  tick: function () {
+      
+    if (!this.raycaster) { return; }  // Not intersecting.
+    
+    let intersection = this.raycaster.components.raycaster.getIntersection(this.el);
+    if (!intersection) { return; }
+    
+    // move the cursor
+    let teleportCursor = document.querySelector('#teleport-cursor');
+    if(teleportCursor) {
+        teleportCursor.setAttribute('position', {x:intersection.point.x, y:intersection.point.y, z:intersection.point.z});
+    }
+    else {
+        console.log(document.querySelector('teleport-cursor'));
+    }
+    
+    console.log(intersection.point);
+  }
+});
